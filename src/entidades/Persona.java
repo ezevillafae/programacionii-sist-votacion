c12b6b0e899dfe1e1ad03886f70d9d06edef3071
ezevillafae <eze.villafae07@gmail.com>
2021-10-17 18:23:18 -0300

    entidad Persona

---
+++ src/entidades/Persona.java
@@ -0,0 +1,47 @@
+package entidades;
+
+public class Persona {
+	private String nombre;
+	private int dni;
+	private int edad;
+	private boolean enfermedad;
+	
+	public Persona(String nombre, int dni, int edad, boolean enfermedad) throws Exception {
+		if(dni <= 0 || nombre == null || edad < 0 || edad >= 120) {
+			throw new Exception();
+		}
+		this.nombre = nombre;
+		this.dni = dni;
+		this.edad = edad;
+		this.enfermedad = enfermedad;
+	}
+	
+	public boolean esMayorDe65() {
+		return this.edad > 65;
+	}
+	
+	public boolean tieneEnfermedadPreexistente() {
+		return this.enfermedad;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Persona : {");
+		sb.append("\n");
+		sb.append("nombre= ").append(nombre);
+		sb.append("\n");
+		sb.append("dni= ").append(dni);
+		sb.append("\n");
+		sb.append("edad= ").append(edad);
+		sb.append("\n");
+		sb.append("enfermedad= ").append(enfermedad);
+		sb.append("\n}\n");
+		return sb.toString();
+	}
+	
+	
+	
+	
+
+}
